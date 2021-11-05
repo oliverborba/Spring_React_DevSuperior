d6959eede8efa04953a5d51256d68d8b3b91292f
Lucas Oliveira <lucas_borba@outlook.com>
2021-11-04 22:27:27 -0300

    Pagination

--- backend/src/main/java/com/borba/dsvendas/service/SaleService.java
+++ backend/src/main/java/com/borba/dsvendas/service/SaleService.java
@@ -4,10 +4,12 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
 import com.borba.dsvendas.dto.SaleDTO;
 import com.borba.dsvendas.entities.Sale;
 import com.borba.dsvendas.repositories.SaleRepository;
+import com.borba.dsvendas.repositories.SellerRepository;
 
 @Service
 public class SaleService {
@@ -15,11 +17,13 @@ public class SaleService {
 	@Autowired
 	private SaleRepository repository;
 
+	private SellerRepository sellerRepository;
+
+	@Transactional(readOnly = true)
 	public Page<SaleDTO> findAll(Pageable pageable) {
+		sellerRepository.findAll();
 		Page<Sale> result = repository.findAll(pageable);
 		return result.map(x -> new SaleDTO(x));
 	}
 
 }
-
-
